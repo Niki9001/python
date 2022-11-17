780c9d22d37eb09c1f56c36d7034ffba3946dd7e
Niki Xiaoning Zheng <101936621+Niki9001@users.noreply.github.com>
2022-11-17 08:41:49 -0400

    Add files via upload
    A very simple small project for calculating repayments

---
+++ Program 2 - Weekly Loan Calculator.py
@@ -0,0 +1,18 @@
+"""
+
+Name: Niki Xiaoning Zheng
+StudentNumber: W0470221
+Assignment 1
+Program 2
+
+"""
+"""
+Program 2 - Weekly Loan Calculator
+"""
+import math
+print("Weekly Loan Calculator")
+a = float(input("Enter the amount of loan: "))
+r = float(input("Enter the interest rate (%): "))
+n = float(input("Enter the number of years: "))
+weeklypayment = (r/5200)/(1-math.pow((1+r/5200),(-52*n)))*a
+print("Your weekly payment will be: ${0:.2f}".format(weeklypayment))
